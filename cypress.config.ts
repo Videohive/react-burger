f81a3c3514e53a136aac1bb6806e98fd369a8dd7
Timur Harchenko <support.solomon@yandex.ru>
2024-07-01 12:30:02 +0400

    resolve conflict

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,5 +5,17 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'http://localhost:3000/',
+    viewportWidth: 1280,  // Ширина окна браузера
+    viewportHeight: 720,  // Высота окна браузера
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
+    viewportWidth: 1280,  // Ширина окна браузера для компонентных тестов
+    viewportHeight: 720,  // Высота окна браузера для компонентных тестов
   },
 });
